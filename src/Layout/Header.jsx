078a19ab989f78b3297cd95cf861dba54c28ad08
Alisa <alisa.luoma@hotmail.fi>
2023-03-15 17:32:03 +0200

    Base for project

---
+++ src/Layout/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../Images/logo.png";
+
+const Header = () => {
+    return (
+        <div
+            className="col-12 py-3 pl-2"
+            style={{ borderBottom: "1px solid #7777" }}
+        >
+            <img
+                src={logo}
+                style={{ height: "35px", verticalAlign: "top" }}
+                alt="React logo"
+            ></img>
+            <span className="h2 pt-4 m-2 text-white-50">
+                TravelOPedia
+            </span>
+        </div>
+    );
+};
+
+export default Header;
