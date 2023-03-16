ae7ba81f42624c63dfb78c073a60270e94e9b21f
Alisa <alisa.luoma@hotmail.fi>
2023-03-16 09:58:56 +0200

    Can delete item

--- src/Components/DestinationList.jsx
+++ src/Components/DestinationList.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { useGetAllDestinationQuery } from "../api/destinationApi";
+import {
+    useGetAllDestinationQuery,
+    useDeleteDestinationMutation,
+} from "../api/destinationApi";
 
 function DestinationList() {
     const { data, isLoading, isSuccess, isError, error } =
         useGetAllDestinationQuery();
+    const [deleteDestination] = useDeleteDestinationMutation();
 
     let content;
     if (isLoading) {
@@ -12,14 +16,33 @@ function DestinationList() {
         console.log(data);
         content = data.map((destination) => {
             return (
-                <article key={destination.id}>
-                    <div className="text-center text-info p-2">
-                        <div>
-                            {destination.city}, {destination.country}-{" "}
-                            {destination.daysNeeded} days
-                        </div>
+                <div
+                    className="row py-1"
+                    key={destination.id}
+                    style={{
+                        borderBottom: "1px solid #3333",
+                        borderTop: "1px solid #333",
+                    }}
+                >
+                    <div className="col-3 offset-3">
+                        {destination.city}, {destination.country}
                     </div>
-                </article>
+                    <div className="col-1 text-warning">
+                        {destination.daysNeeded} days
+                    </div>
+                    <div className="col-2">
+                        <button
+                            className="btn btn-danger"
+                            onClick={() =>
+                                deleteDestination({
+                                    id: destination.id,
+                                })
+                            }
+                        >
+                            Remove
+                        </button>
+                    </div>
+                </div>
             );
         });
     } else if (isError) {
